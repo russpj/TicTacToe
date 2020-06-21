cc4eae57d35bad38acb771c4b8d3223d4e6002ec
Russ Paul-Jones <russ@paul-jones.org>
2020-06-21 16:31:48 -0700

    Add project files.

---
+++ TicTacToe.py
@@ -0,0 +1,38 @@
+"""
+TicTacToe
+
+Contains the engines for a Tic-Tac-Toe board
+"""
+
+def IsWinner(board, team):
+	"""
+	Input
+		board: a double list of the squares on a Tic-Tac-Toe baord
+		team: X or O
+
+		returns True iff team has won
+	"""
+	for row in range(3):
+		allTeam = True
+		for col in range(3):
+			if board[row][col] != team:
+				allTeam = False
+	if allTeam:
+		return True
+	
+	for col in range(3):
+		allTeam = True
+		for row in range(3):
+			if board[row][col] != team:
+				allTeam = False
+	if allTeam:
+		return True
+
+	allTeam = True
+	for index in range(3):
+		if board[index][index] != team:
+			allTeam = False
+	if allTeam:
+		return True
+
+	return board[0][2] == team and board[1][1] == team and board[2][0]
