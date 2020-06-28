347bf0ee560db7e71b0758b63edfb57326395309
Russ Paul-Jones <russ@paul-jones.org>
2020-06-27 17:39:30 -0700

    Add game.py
    This will house the game play. We may need to factor out some of the board printing from testmain.py

---
+++ game.py
@@ -0,0 +1,17 @@
+"""
+This handles the game play of Tic-Tac-Toe
+"""
+
+
+from TicTacToe import IsWinner
+from TicTacToe import ValidateMove
+from TicTacToe import Move
+from TicTacToe import MoveValidation
+
+
+def Play():
+	print('Would you like to play a game?')
+	return
+
+if __name__ == '__main__':
+	Play()
