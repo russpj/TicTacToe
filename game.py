d2f48bc7e5d9aa3aecbf84df26caa797d9c95282
Russ Paul-Jones <russ@paul-jones.org>
2020-06-30 11:53:11 -0700

    Implement 2-person play
    Known bug: Doesn't recognize cat's game and will probably go into infinite loop.

--- game.py
+++ game.py
@@ -16,18 +16,47 @@ def StringFromBoard(board):
 	return '\n-----\n'.join(rows)
 
 
+def PlayTicTacToe(numPlayers):
+	numberBoard = [
+			['0', '1', '2'],
+			['3', '4', '5'],
+			['6', '7', '8']
+		]
+	print('Thank you. The board is numbered like this:')
+	print(StringFromBoard(numberBoard))
+	turn = 'X'
+	board = [
+			[' ', ' ', ' '],
+			[' ', ' ', ' '],
+			[' ', ' ', ' ']
+		]
+	nextMover = 'X'
+	while True:
+		while True:
+			move = int(input('Tell me your move, {}: '.format(nextMover)))
+			result = ValidateMove(board, nextMover, move)
+			if result == MoveValidation.Valid:
+				break
+		Move(board, nextMover, move)	
+		print(StringFromBoard(board))
+		if IsWinner(board, nextMover):
+			print ('{} is the Winner!'.format(nextMover))
+			break
+		if nextMover == 'X':
+			nextMover = 'O'
+		else:
+			nextMover = 'X'
+
 def Play():
-	gameName = input('Would you like to play a game, Professor? ')
-	if gameName == 'TicTacToe':
-		numberBoard = [
-				['0', '1', '2'],
-				['3', '4', '5'],
-				['6', '7', '8']
-			]
-		print('Thank you. The board is numbered like this:')
-		print(StringFromBoard(numberBoard))
-	else:
-		print("I don't want to play {}".format(gameName))
+	while True:
+		gameName = input('Would you like to play a game, Professor? ')
+		if gameName == 'TicTacToe':
+			PlayTicTacToe(2)
+		elif gameName == "No":
+			print ('Good-Bye, Professor')
+			break
+		else:
+			print("I don't know how to play {}".format(gameName))
 	return
 
 if __name__ == '__main__':
